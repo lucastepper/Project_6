d474220dfe4a07c5e7e95adcbc7d4f535be2e32f
Yaoyi Chen <29800192+yaoyic@users.noreply.github.com>
2018-12-11 14:08:01 +0100

    LJ-related force and potential calc (prototype), can be used for next step before further optimization.

---
+++ LJ.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+# temporarily define all the constants here
+epsilon_lj = 1.
+sigma_lj = 1.
+cutoff_lj = 3. * sigma_lj
+
+def LJ_potential(q):
+    """Calculate the system Lennard-Jones potential based on given particle coordinates.
+    
+    Input:
+        q (iterable of (#dimension,)-arrays): list or array of particle coordinates.
+    
+    Output:
+        pot (float): sum of all pairwise LJ potentials.    
+    """
+    
+    # TODO: input check
+    
+    n_particle = np.shape(q)[0]
+    pot = 0.
+    for i in range(n_particle - 1):
+        for j in range(1, n_particle):
+            pot += _LJ_potential_pair(q[i], q[j])
+    return pot
+
+def _LJ_potential_pair(qi, qj):
+    """Calculate pairwise Lennard-Jones potential.
+        (require global epsilon_lj, sigma_lj, cutoff_lj)
+    """
+    rij = np.linalg.norm(qi - qj)
+    if rij <= 0 or rij > cutoff_lj:
+        return 0.
+    return 4 * epsilon_lj * (sigma_lj ** 12 / rij ** 12 - sigma_lj ** 6 / rij ** 6)
+
+def LJ_force(q, k):
+    """Calculate the system Lennard-Jones force on particle *i* based on given particle coordinates.
+    
+    Input:
+        q (iterable of (#dimension,)-arrays): list or array of particle coordinates.
+        k (int): index of the particle for force calc.
+    
+    Output:
+        force ((#dimension,)-array): system Lennard-Jones force on particle *i*.    
+    """
+    
+    # TODO: input check
+    
+    n_particle = np.shape(q)[0]
+    ndim = np.shape(q)[1]
+    force = np.zeros(ndim)
+    for j in range(n_particle):
+        # pairwise force calc will return 0 for identical two particles.
+        force += _LJ_force_pair(q[k], q[j])
+    return force
+
+def _LJ_force_pair(qk, qj):
+    """Calculate pairwise Lennard-Jones force on paricle *k*.
+        (require global epsilon_lj, sigma_lj, cutoff_lj)
+    """
+    rjk = np.linalg.norm(qj - qk)
+    ndim = np.shape(qk)[0]
+    if rjk <= 0 or rjk > cutoff_lj:
+        return np.zeros(ndim)
+    return 4 * epsilon_lj * (12 * sigma_lj ** 12 / rjk ** 14 - 6 * sigma_lj ** 6 / rjk ** 8) * (qj - qk)
